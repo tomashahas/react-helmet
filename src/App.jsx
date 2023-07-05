4f41603c24592400e915a0498757aa2be89df522
tomashahas <133536265+tomashahas@users.noreply.github.com>
2023-07-05 18:04:31 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import Home from './Home'
 import './style.css';
 import { HelmetProvider } from 'react-helmet-async';
 
+const helmetContext = {};
+
 const App = () => {
     return (
-        <HelmetProvider>
+        <HelmetProvider context={helmetContext}>
             <BrowserRouter>
                 <Routes>
                     <Route path='/' Component={Home}></Route>
