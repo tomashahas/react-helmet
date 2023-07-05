c0b4b962e9beb6131f012fd287723f62b1a62f3f
tomashahas <133536265+tomashahas@users.noreply.github.com>
2023-07-05 17:57:45 +0200

    Add files via upload

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import { Helmet } from 'react-helmet';
 
 const Contact = () => {
-    <Helmet>
-      <link rel="stylesheet" href="/src/main.css" />
-    </Helmet>
   return (
     <>
-
-
     <div>Contact</div>
     </>
   )
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
+    <>
+    <Helmet>
+      <link rel="stylesheet" href="/src/main.css" />
+    </Helmet>
+
     <div>Home</div>
+    <Link to="/contact">contact</Link>
+
+    </>
   )
 }
 
